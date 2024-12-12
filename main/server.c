f08551117431544640ee15e70a20efd111a4adec
Milan Vodák <milanvodak02@gmail.com>
2024-12-12 21:16:24 +0100

    feat: serve files from filesystem

--- main/server.c
+++ main/server.c
@@ -8,13 +8,29 @@
 #include "esp_err.h"
 #include "esp_http_server.h"
 #include "esp_log.h"
+#include "esp_spiffs.h"
 #include "http_parser.h"
 #include "metronome.h"
 #include "sys/param.h"
 #include <errno.h>
+#include <stddef.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 static const char* TAG = "http";
+
+static void fs_init()
+{
+    // Iniitialize and mount the filesystem partition
+    esp_vfs_spiffs_conf_t config = {
+        .base_path = SERVER_BASE_PATH,
+        .partition_label = NULL,
+        .max_files = 5,
+        .format_if_mount_failed = false,
+    };
+    ESP_ERROR_CHECK(esp_vfs_spiffs_register(&config));
+}
 
 static void receive_data(httpd_req_t* req, uint max_len, char* dest)
 {
@@ -41,6 +57,48 @@ static int receive_int(httpd_req_t* req, uint max_digits)
 static esp_err_t hello_handler(httpd_req_t* req)
 {
     httpd_resp_send(req, "Hello world", HTTPD_RESP_USE_STRLEN);
+    return ESP_OK;
+}
+
+static esp_err_t get_handler(httpd_req_t* req)
+{
+    ESP_LOGI(TAG, "uri: %s", req->uri);
+
+    // Combine base path with requested file path
+    size_t path_len = strlen(SERVER_BASE_PATH) + strlen(req->uri) + 1;
+    char path[path_len];
+    strcpy(path, SERVER_BASE_PATH);
+    strcat(path, req->uri);
+    path[path_len] = '\0';
+
+    // Try to open the file
+    FILE* f = fopen(path, "r");
+    if (f == NULL) {
+        ESP_LOGE(TAG, "failed to open file: %s", path);
+        httpd_resp_send_404(req);
+        return ESP_FAIL;
+    }
+
+    // Read the file and send it in chunks
+    char chunk[SERVER_CHUNK_SIZE];
+    size_t read_bytes;
+    do {
+        read_bytes = fread(chunk, 1, SERVER_CHUNK_SIZE, f);
+
+        if (read_bytes > 0) {
+            if (httpd_resp_send_chunk(req, chunk, read_bytes) != ESP_OK) {
+                fclose(f);
+                ESP_LOGE(TAG, "failed to send file");
+                httpd_resp_sendstr_chunk(req, NULL);
+                httpd_resp_send_err(req, HTTPD_500_INTERNAL_SERVER_ERROR, "Failed to send file");
+                return ESP_FAIL;
+            }
+        }
+    } while (read_bytes != 0);
+
+    fclose(f);
+    ESP_LOGI(TAG, "file sent");
+    httpd_resp_send_chunk(req, NULL, 0);
     return ESP_OK;
 }
 
@@ -76,6 +134,12 @@ static const httpd_uri_t hello = {
     .handler = hello_handler,
 };
 
+static const httpd_uri_t get_uri = {
+    .uri = "/*",
+    .method = HTTP_GET,
+    .handler = get_handler,
+};
+
 static const httpd_uri_t set_bpm_patch_uri = {
     .uri = "/set-bpm",
     .method = HTTP_PATCH,
@@ -90,14 +154,18 @@ static const httpd_uri_t set_volume_patch_uri = {
 
 void server_init()
 {
+    fs_init();
+
     httpd_handle_t server;
     httpd_config_t config = HTTPD_DEFAULT_CONFIG();
+    config.uri_match_fn = httpd_uri_match_wildcard;
 
     ESP_ERROR_CHECK(httpd_start(&server, &config));
     ESP_LOGI(TAG, "HTTP server started");
 
     ESP_LOGI(TAG, "Registering URI handlers");
     httpd_register_uri_handler(server, &hello);
+    httpd_register_uri_handler(server, &get_uri);
     httpd_register_uri_handler(server, &set_bpm_patch_uri);
     httpd_register_uri_handler(server, &set_volume_patch_uri);
 }
--- main/server.h
+++ main/server.h
@@ -4,4 +4,7 @@
  * @author Milan Vodák <xvodak07@stud.fit.vut.cz>
  */
 
+#define SERVER_BASE_PATH "/www"
+#define SERVER_CHUNK_SIZE 8192
+
 void server_init();
