a6459efb83b6c6f97987a480c6fe30e77766562c
Milan Vodák <milanvodak02@gmail.com>
2024-12-12 18:54:29 +0100

    feat: use PWM to set metronome volume

--- main/metronome.c
+++ main/metronome.c
@@ -6,8 +6,11 @@
 
 #include "metronome.h"
 #include "driver/gpio.h"
+#include "driver/ledc.h"
+#include "esp_err.h"
 #include "esp_log.h"
 #include "freertos/idf_additions.h"
+#include "hal/ledc_types.h"
 #include "portmacro.h"
 
 static const char* TAG = "metronome";
@@ -15,11 +18,36 @@ static const char* TAG = "metronome";
 static int period_ms;
 static int beat_length_ms;
 
+void pwm_init()
+{
+    ledc_timer_config_t timer_config = {
+        .speed_mode = PWM_SPEED_MODE,
+        .duty_resolution = PWM_DUTY_RES,
+        .timer_num = PWM_TIMER,
+        .freq_hz = PWM_FREQUENCY,
+        .clk_cfg = LEDC_AUTO_CLK,
+    };
+    ESP_ERROR_CHECK(ledc_timer_config(&timer_config));
+
+    ledc_channel_config_t channel_config = {
+        .gpio_num = CONFIG_PWM_PIN,
+        .speed_mode = PWM_SPEED_MODE,
+        .channel = PWM_CHANNEL,
+        .intr_type = LEDC_INTR_DISABLE,
+        .timer_sel = PWM_TIMER,
+        .duty = 0,
+        .hpoint = 0,
+    };
+    ESP_ERROR_CHECK(ledc_channel_config(&channel_config));
+}
+
 void metronome_init()
 {
+    pwm_init();
     gpio_reset_pin(CONFIG_GPIO_PIN);
     gpio_set_direction(CONFIG_GPIO_PIN, GPIO_MODE_OUTPUT);
     metronome_set_bpm(DEFAULT_BPM);
+    metronome_set_volume(DEFAULT_VOLUME);
 }
 
 void metronome_set_bpm(unsigned int bpm)
@@ -31,6 +59,16 @@ void metronome_set_bpm(unsigned int bpm)
     // Calculate short enough beat length
     beat_length_ms = period_ms / 4;
     ESP_LOGI(TAG, "beat length: %d ms", beat_length_ms);
+}
+
+void metronome_set_volume(unsigned int volume)
+{
+    ESP_LOGI(TAG, "setting volume to %d", volume);
+    // Quadratic scaling
+    int duty = PWM_MAX_DUTY * (volume / 100.0) * (volume / 100.0);
+    ESP_LOGI(TAG, "duty: %d", duty);
+    ledc_set_duty(PWM_SPEED_MODE, PWM_CHANNEL, duty);
+    ledc_update_duty(PWM_SPEED_MODE, PWM_CHANNEL);
 }
 
 static void metronome_beat()
--- main/metronome.h
+++ main/metronome.h
@@ -5,7 +5,16 @@
  */
 
 #define DEFAULT_BPM 120
+#define DEFAULT_VOLUME 50
+
+#define PWM_SPEED_MODE LEDC_HIGH_SPEED_MODE
+#define PWM_CHANNEL LEDC_CHANNEL_0
+#define PWM_TIMER LEDC_TIMER_0
+#define PWM_FREQUENCY 3500
+#define PWM_DUTY_RES LEDC_TIMER_13_BIT
+#define PWM_MAX_DUTY 6144 // 75 % of 2 ** 13
 
 void metronome_init();
 void metronome_start();
 void metronome_set_bpm(unsigned int bpm);
+void metronome_set_volume(unsigned int volume);
